0dbbb755265bb3c135c164ebd9e93e602bbd74ec
Beca <234265987+becsnr@users.noreply.github.com>
2026-01-15 09:28:06 -0300

    armazenar os livros no array
    criei um array, e toda vez que clicar no botão vai adicionar os dados do livro dentro desse array

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ const addBook = document.getElementById('add-book');
 const divBooks = document.getElementById('list-books');
 const card = document.querySelectorAll('.card');
 
+let storedBooks = [];
 
 addBook.addEventListener("click", () => {
     let book = bookInput.value;
 
-    // criar o card
-    const newCard = document.createElement('div');
-    newCard.className = 'card';
+    const dadosBook = {
+        title: book, 
+        read: false
+    };
+    storedBooks.push(dadosBook);
 
-    // criar h2
-    const titleBook = document.createElement('h2');
-    titleBook.className = 'title-book';
-    titleBook.textContent = book;
-
-    // coloca o h2 dentro do card
-    newCard.appendChild(titleBook);
-
-    // coloca o card dentro da div
-    divBooks.appendChild(newCard);
+    criarCards();
+    console.log(storedBooks);
 });
+
+function criarCards() {
+    divBooks.innerHTML = "";
+
+    storedBooks.forEach(book => {
+        // criar o card
+        const newCard = document.createElement('div');
+        newCard.className = 'card';
+
+        // criar h2
+        const titleBook = document.createElement('h2');
+        titleBook.className = 'title-book';
+        titleBook.textContent = book.title;
+
+        // coloca o h2 dentro do card
+        newCard.appendChild(titleBook);
+
+        // coloca o card dentro da div
+        divBooks.appendChild(newCard);
+    });
+};
