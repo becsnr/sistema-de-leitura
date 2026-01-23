2a4c85d8863e88f29d275344596c240ef9e73094
Beca <234265987+becsnr@users.noreply.github.com>
2026-01-23 09:14:14 -0300

    localStorage e renderizar

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const bookInput = document.getElementById('book');
 const addBook = document.getElementById('add-book');
 const divBooks = document.getElementById('list-books');
 
-let storedBooks = [];
+let storedBooks = JSON.parse(localStorage.getItem("dados")) || [];
+renderizar();
 
 addBook.addEventListener("click", () => {
     let book = bookInput.value;
@@ -13,57 +14,63 @@ addBook.addEventListener("click", () => {
         read: false
     };
     storedBooks.push(dadosBook);
+    localStorage.setItem("dados", JSON.stringify(storedBooks));
 
-    criarCards();
+    renderizar();
     console.log(storedBooks);
 });
 
-function criarCards() {
+function criarCards(book, indice) {
+    // criar o card
+    const newCard = document.createElement('div');
+    newCard.className = 'card';
+
+    // criar h2
+    const titleBook = document.createElement('h2');
+    titleBook.className = 'title-book';
+    titleBook.textContent = book.title;
+
+    // botoes
+    const concluirBtn = document.createElement('button');
+    concluirBtn.className = 'button-card finish';
+    concluirBtn.textContent = 'Concluir Leitura';
+
+    concluirBtn.addEventListener("click", () => {
+        storedBooks[indice].read = true;
+        localStorage.setItem("dados", JSON.stringify(storedBooks));
+        renderizar();
+    });
+
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'button-card remove';
+    removeBtn.textContent = 'Remover';
+
+    removeBtn.addEventListener("click", () => {
+        storedBooks.splice(indice, 1);
+        localStorage.setItem("dados", JSON.stringify(storedBooks));
+        renderizar();
+    });
+
+    if (book.read) {
+        newCard.style.background = '#d5f2d9a8';
+
+        concluirBtn.style.display = 'none';
+        removeBtn.style.display = 'none';
+    };
+
+    // coloca o h2 e botoes dentro do card
+    newCard.appendChild(titleBook);
+    newCard.appendChild(concluirBtn);
+    newCard.appendChild(removeBtn);
+
+    // coloca o card dentro da div
+    divBooks.appendChild(newCard);
+};
+
+function renderizar() {
     divBooks.innerHTML = "";
 
     storedBooks.forEach((book, indice) => {
-        // criar o card
-        const newCard = document.createElement('div');
-        newCard.className = 'card';
-
-        // criar h2
-        const titleBook = document.createElement('h2');
-        titleBook.className = 'title-book';
-        titleBook.textContent = book.title;
-
-        // botoes
-        const concluirBtn = document.createElement('button');
-        concluirBtn.className = 'button-card finish';
-        concluirBtn.textContent = 'Concluir Leitura';
-
-        concluirBtn.addEventListener("click", () => {
-            storedBooks[indice].read = true;
-            criarCards();
-            
-        });
-
-        const removeBtn = document.createElement('button');
-        removeBtn.className = 'button-card remove';
-        removeBtn.textContent = 'Remover';
-
-        removeBtn.addEventListener("click", () => {
-            storedBooks.splice(indice, 1);
-            criarCards();
-        });
-
-        if (book.read) {
-            newCard.style.background = '#d5f2d9a8';
-
-            concluirBtn.style.display = 'none';
-            removeBtn.style.display = 'none';
-        };
-
-        // coloca o h2 e botoes dentro do card
-        newCard.appendChild(titleBook);
-        newCard.appendChild(concluirBtn);
-        newCard.appendChild(removeBtn);
-
-        // coloca o card dentro da div
-        divBooks.appendChild(newCard);
+        criarCards(book, indice);
     });
 };
